a223f975b6c690a9983f262b7874ef76e242ea6c
Alexandre Tessier <alexandre.tessier6@sympatico.ca>
2018-11-29 14:22:12 -0500

    Adding files

--- Final_BaseProject/Inc/stm32l4xx_it.h
+++ Final_BaseProject/Inc/stm32l4xx_it.h
@@ -55,6 +55,7 @@ void UsageFault_Handler(void);
 void DebugMon_Handler(void);
 void SysTick_Handler(void);
 void TIM1_TRG_COM_TIM17_IRQHandler(void);
+void EXTI15_10_IRQHandler(void);
 
 #ifdef __cplusplus
 }
